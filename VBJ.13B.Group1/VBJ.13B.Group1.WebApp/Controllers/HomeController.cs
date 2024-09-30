85a6c13e696e4db3348efced70e8005f1778000a
Bence Csuzdi <csuzdibence@gmail.com>
2024-09-30 10:35:31 +0200

    Created the ASP.NET MVC tutorial WebApp

---
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VBJ._13B.Group1.WebApp.Models;
+
+namespace VBJ._13B.Group1.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
