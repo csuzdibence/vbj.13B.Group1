d904f119f64e493a9ce01f60d6e6cdbc06e3869b
Bence Csuzdi <csuzdibence@gmail.com>
2024-10-07 10:33:51 +0200

    Subtraction logic added

--- VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/CalculatorController.cs
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/CalculatorController.cs
@@ -16,7 +16,19 @@ namespace VBJ._13B.Group1.WebApp.Controllers
             // logic
             int sum = number1 + number2;
             // store logic result
-            ViewBag.Result = sum;
+            ViewBag.Result = "Az összeadás eredménye: " + sum;
+
+            // present result
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Subtract(int number1, int number2)
+        {
+            // logic
+            int subtraction = number1 - number2;
+            // store logic result
+            ViewBag.Result = "A kivonás eredménye: " + subtraction;
 
             // present result
             return View("Index");
