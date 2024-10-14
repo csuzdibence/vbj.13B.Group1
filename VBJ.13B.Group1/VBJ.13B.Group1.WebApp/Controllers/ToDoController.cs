5d6af479e0c37a072a4fb89c67c6deb741eb226a
Bence Csuzdi <csuzdibence@gmail.com>
2024-10-14 09:52:42 +0200

    ToDo exercise has been started

---
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/ToDoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VBJ._13B.Group1.WebApp.Models;
+
+namespace VBJ._13B.Group1.WebApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private static List<ToDoItem> toDoItems = new List<ToDoItem>();
+
+        public ToDoController()
+        { 
+            // ha nincs benne elem, akkor rakjunk bele (kamu adatbázis)
+            if (!toDoItems.Any())
+            {
+                toDoItems.Add(new ToDoItem()
+                {
+                    ID = 1,
+                    IsCompleted = false,
+                    Title = "Mosni"
+                });
+                toDoItems.Add(new ToDoItem()
+                {
+                    ID = 2,
+                    IsCompleted = true,
+                    Title = "Felkészülni az órára"
+                });
+                toDoItems.Add(new ToDoItem()
+                {
+                    ID = 3,
+                    IsCompleted = false,
+                    Title = "Főzés"
+                });
+            }
+        }
+
+        /// <summary>
+        /// Ez a végpont a /ToDo url-en lesz elérhető
+        /// </summary>
+        /// <returns>A ToDo (View) nézetet</returns>
+        public IActionResult Index()
+        {
+            return View(toDoItems);
+        }
+
+        // /ToDo/Create url
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // /ToDo/Edit
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete() 
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Models/ToDoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace VBJ._13B.Group1.WebApp.Models
+{
+    /// <summary>
+    /// Ez az osztály reprezentál egy elkészítendő teendőt
+    /// </summary>
+    public class ToDoItem
+    {
+        /// <summary>
+        /// Ez a property állítja az azonosítót
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Ez a property állítja a teendő címét
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Azt jelöli, hogy a teendő elkészült-e
+        /// </summary>
+        public bool IsCompleted { get; set; }
+    }
+}
