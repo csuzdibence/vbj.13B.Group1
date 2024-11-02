eca443f5cee25014775de6f49a977448b9b5ce85
Csuzdi, Bence (ADV D EU HU OPS 3 AS 3) <bence.csuzdi@evosoft.com>
2024-11-02 16:52:51 +0100

    Football Fantasy exercise is added to the project

---
+++ VBJ.13B.Group1/FootballFantasy/Models/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace FootballFantasy.Models
+{
+    /// <summary>
+    /// Ez az osztály egy futball játékost reprezentál.
+    /// </summary>
+    public class Player
+    {
+        private int jerseyNumber;
+
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Ezen tulajdonságon keresztül a játékos mezszáma állítható.
+        /// </summary>
+        public int JerseyNumber
+        {
+            get => jerseyNumber;
+            set
+            {
+                if (value <= 0 || value >= 100)
+                {
+                    throw new Exception("A mezszám csak 1 és 100 közötti lehet!");
+                }
+
+                jerseyNumber = value;
+            }
+        }
+    }
+}
---
+++ VBJ.13B.Group1/FootballFantasy/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿namespace FootballFantasy.Models
+{
+    /// <summary>
+    /// Ez az osztály egy futball csapatot reprezentál.
+    /// </summary>
+    public class Team
+    {
+        public Team()
+        {
+            Players = new List<Player>();
+        }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Player> Players { get; set; }
+    }
+}
