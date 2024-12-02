2b74c1124b0cfc019205ac12fedc074f965ebc43
Bence Csuzdi <csuzdibence@gmail.com>
2024-12-02 10:40:40 +0100

    Added ef codes

---
+++ VBJ.13B.Group1/Students.Model/Implementations/StudentManagers/InMemoryDecorator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students.Model.Implementations.StudentManagers
+{
+    internal class InMemoryDecorator
+    {
+    }
+}
---
+++ VBJ.13B.Group1/Students.Model/StudentDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Students.Model
+{
+    public class StudentDbContext : DbContext
+    {
+        // Ez egy táblát reprezentál
+        public DbSet<Student> Students { get; set; }
+
+        // Örökölve van a DbContext osztály -> EF
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Ha még nincs konfigurálva az adatbázis
+            if (!optionsBuilder.IsConfigured)
+            {
+                // akkor megadjuk a connection stringet (első paraméter: szerver, második: adatbázis név
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=StudentDatabase");
+            }
+        }
+    }
+}
